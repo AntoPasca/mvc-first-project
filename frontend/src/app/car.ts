7862925e60bf9a4e491ca6bd8b556f5425a5550c
Antonio Pasca <a.pasca@almaviva.it>
2019-06-13 16:08:13 +0200

    delete

--- frontend/src/app/car.ts
+++
@@ -1,6 +0,0 @@
-export class Car {
-    id: number;
-    model: string;
-    targa: string;
-    cilindrata: number;
-}
--- frontend/src/app/customer.ts
+++
@@ -1,7 +0,0 @@
-export class Customer {
-    id: number;
-    name: string;
-    age: number;
-    password: string;
-    active: boolean;
-}
